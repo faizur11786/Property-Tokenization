8d957d6e4c3899b5c38fd8ffbb8d796aec1058b7
Faizur Rahman <faizur11786@gmail.com>
2022-03-31 20:01:22 +0400

    some test sace done

--- contracts/PropertyNFT.sol
+++ contracts/PropertyNFT.sol
@@ -9,18 +9,21 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./PropertyTokenization.sol";
 
-
 interface IPropertyToken {
-    function buyToken(uint256, address, address) external returns(bool);
+    function buyToken(
+        uint256,
+        address,
+        address
+    ) external returns (bool);
 }
 
-
-
-contract PropertyNFT is Ownable{
+contract PropertyNFT is Ownable {
     using Counters for Counters.Counter;
     Counters.Counter public tokenIds;
 
-    mapping (address => address) public balanceFor;
+    mapping(address => address) public balanceFor;
+
+    mapping(address => uint256) public idOf;
 
     address[] public paymentMethods;
 
@@ -34,9 +37,21 @@ contract PropertyNFT is Ownable{
         address propertyAddress;
     }
 
-    mapping ( uint256 => Properties ) public properties;
+    mapping(uint256 => Properties) public properties;
 
-    constructor () Ownable(){}
+    event CreateProperty(
+        address indexed _propertyAddress,
+        uint256 _propTotalSupply
+    );
+
+    event BuyShares(
+        address indexed _token,
+        address indexed _to,
+        address indexed __buyWithToken,
+        uint256 _value
+    );
+
+    constructor() Ownable() {}
 
     function listProperty(
         uint256 _totalSupply,
@@ -46,7 +61,7 @@ contract PropertyNFT is Ownable{
         bytes memory _propURI,
         address _propOwner,
         bool _saleState
-    ) external onlyOwner returns(bool success){
+    ) external onlyOwner returns (bool success) {
         tokenIds.increment();
         uint256 newTokenId_ = tokenIds.current();
         PropertyTokenization propToken_ = new PropertyTokenization(
@@ -57,54 +72,100 @@ contract PropertyNFT is Ownable{
             _saleState
         );
         properties[newTokenId_] = Properties({
-            propId:_propId,
-            propName:_propName,
-            propSymbol:_propSymbol,
-            propertyURI:_propURI,
-            propTotalSupply:_totalSupply,
-            propOwner:_propOwner,
+            propId: _propId,
+            propName: _propName,
+            propSymbol: _propSymbol,
+            propertyURI: _propURI,
+            propTotalSupply: _totalSupply,
+            propOwner: _propOwner,
             propertyAddress: address(propToken_)
+        });
+        idOf[address(propToken_)] = newTokenId_;
+        emit CreateProperty(address(propToken_), _totalSupply);
+        return true;
+    }
+
+    function burn(uint256 _tokenId) external onlyOwner returns (bool) {
+        require(_exists(_tokenId), "Token does not exist");
+        properties[_tokenId] = Properties({
+            propId: "",
+            propName: "",
+            propSymbol: "",
+            propertyURI: "",
+            propTotalSupply: 0,
+            propOwner: address(0),
+            propertyAddress: address(0)
         });
         return true;
     }
 
-
     function buyShares(
-        address _token, 
-        uint256 _amount, 
+        address _token,
+        uint256 _amount,
         address _buyWithToken
-    ) public virtual returns(bool success){
+    ) public virtual returns (bool success) {
+        uint256 tokenId = idOf[_token];
+        require(_exists(tokenId), "Token does not exist");
         IPropertyToken propertyToken = IPropertyToken(_token);
         propertyToken.buyToken(_amount, _msgSender(), _buyWithToken);
         balanceFor[_msgSender()] = _token;
+        emit BuyShares(_token, _msgSender(), _buyWithToken, _amount);
         return true;
     }
 
-    function setBalanceFor(address _to, address _with) external{
+    function setBalanceFor(address _to, address _with) external {
         balanceFor[_to] = _with;
     }
 
-    function paymentMethodLength() public view virtual returns (uint256){
+    function paymentMethodLength() public view virtual returns (uint256) {
         return paymentMethods.length;
     }
 
-    function addPaymentMethod(address newPaymentMethod) public onlyOwner returns (bool success){
-        require(newPaymentMethod != address(0), "Invalid address");
-        for(uint256 i = 0; i < paymentMethodLength(); i++){
-            require(paymentMethods[i] != newPaymentMethod, "Duplicate Payment Method");
+    function addPaymentMethod(address[] memory newPaymentMethod)
+        public
+        virtual
+        onlyOwner
+        returns (bool success)
+    {
+        for (uint256 i = 0; i < newPaymentMethod.length; i++) {
+            require(newPaymentMethod[i] != address(0), "Invalid address");
+            _addPaymentMethod(newPaymentMethod[i]);
         }
-        paymentMethods.push(newPaymentMethod);
         return true;
     }
 
-
-
-    function getTokenURI(uint256 _tokenId) public view virtual returns (string memory){
-        require(_exists(_tokenId), "URI query for nonexistent token");
-        return string(properties[_tokenId].propertyURI);       
+    function _addPaymentMethod(address newPaymentMethod)
+        internal
+        returns (bool success)
+    {
+        if (paymentMethods.length == 0) {
+            paymentMethods.push(newPaymentMethod);
+        } else {
+            for (uint256 i = 0; i < paymentMethods.length; i++) {
+                if (paymentMethods[i] != newPaymentMethod) {
+                    paymentMethods.push(newPaymentMethod);
+                    return true;
+                }
+            }
+        }
     }
 
-    function setTokenURI(uint256 _tokenId, bytes memory _data) public view onlyOwner returns (bool){
+    function getTokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        returns (string memory)
+    {
+        require(_exists(_tokenId), "URI query for nonexistent token");
+        return string(properties[_tokenId].propertyURI);
+    }
+
+    function setTokenURI(uint256 _tokenId, bytes memory _data)
+        public
+        view
+        onlyOwner
+        returns (bool)
+    {
         require(_exists(_tokenId), "URI query for nonexistent token");
         Properties memory property = properties[_tokenId];
         property.propertyURI = _data;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,18 @@
-require("dotenv").config();
-
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
+require( "@nomiclabs/hardhat-etherscan" );
+require( "@nomiclabs/hardhat-waffle" );
+require( "hardhat-gas-reporter" );
+require( "solidity-coverage" );
+require( "dotenv" ).config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task( "accounts", "Prints the list of accounts", async ( taskArgs, hre ) => {
   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
+  for ( const account of accounts ) {
+    console.log( account.address );
   }
-});
+} );
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -22,19 +21,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+  networks: {
+    ganache: {
+      url: process.env.GANACHE_URL,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 31337
+    }
   },
 };
